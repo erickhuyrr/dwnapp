9fdc50ec1557288ecd3bc35ef7fee7a646793f0f
Erick <zenopubg011@gmail.com>
2025-11-08 17:56:01 +0545

    Create service-worker.js

---
+++ www/service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'media-downloader-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/noti.html'
+];
+
+// Install event - cache resources
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// Activate event - cleanup old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch event - serve from cache, fallback to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
